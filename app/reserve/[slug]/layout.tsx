de33b07f7abb316ebc0bde878547ce4346647157
Ofir Stiber Voronzov <ofir@Dueto.io>
2023-05-02 10:51:48 +0300

    Connected project to DB Added fetching to restaurant page

---
+++ app/reserve/[slug]/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+	title: 'OpenTable | Reservation',
+	description: '',
+};
+
+export default function ReservationLayout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>;
+}
